4d6f93991e0980f541c45ce8a97e0bcf4b193c15
Gustavo Ito <gustavoito223@gmail.com>
2024-08-12 08:45:06 -0300

    shared

---
+++ Ex/primeiroeUltimo.java
@@ -0,0 +1,27 @@
+public class primeiroeUltimo {
+    public static void main(String[] args){
+        System.out.println(isShared(88, 71));
+    }
+    public static boolean isShared(int n1, int n2){
+        if(n1 < 0 || n1 > 99 || n2 < 0 || n2 > 99){
+            return false;
+        }
+        int primeiroDigiton1 = 0;
+        int n10 = n1;
+        while(n10 > 0){
+            primeiroDigiton1 = n10 % 10;
+            n10 /= 10;
+        }
+        int primeiroDigiton2 = 0;
+        int n20 = n2;
+        while(n20 > 0){
+            primeiroDigiton2 = n20 % 10;
+            n20 /= 10;
+        }
+        if(primeiroDigiton1 == primeiroDigiton2 || primeiroDigiton1 == n2 % 10 || n1 % 10 == primeiroDigiton2 || n1 % 10 == n2 % 10){
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
---
+++ Ex/shareNum.java
@@ -0,0 +1,14 @@
+public class shareNum {
+    public static void main(String[] args){
+        System.out.println(isShared(123, 123));
+    }
+    public static boolean isShared(int n1, int n2){
+        int n10 = n1;
+        int n20 = n2;
+        int temp = 1;
+        while(n1 > 0){
+            temp = n10 % 10;
+            if()
+        }
+    }
+}
