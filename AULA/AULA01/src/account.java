df482cbeb27de59b9284a84a01113e264a4625f6
Gustavo Ito <gustavoito223@gmail.com>
2024-08-21 08:11:49 -0300

    aula

---
+++ AULA/AULA01/src/account.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class account {
+    public static void main(String[] args){
+        dados dados1 = new dados("s", 10);
+        System.out.printf("%s saldo: %.2f\n", dados1.getNome(), dados1.getSaldo());
+        Scanner input = new Scanner(System.in);
+        System.out.printf("Digite quanto quer depositar: R$");
+        double depositarSaldo = input.nextDouble();
+        System.out.printf("+%.2f\n", depositarSaldo);
+        dados1.depositar(depositarSaldo);
+        System.out.printf("%s saldo: %.2f\n", dados1.getNome(), dados1.getSaldo());
+    }
+}
--- AULA/AULA01/src/dados.java
+++ AULA/AULA01/src/dados.java
@@ -1,6 +1,29 @@
 public class dados {
-    public usuario(){
-        String nome;
-        int saldo;
+    private String nome;
+    private double saldo;
+
+    public dados(String nome, double saldo){
+        this.nome = nome;
+        if(saldo > 0){
+            this.saldo = saldo;
+        }
+    }
+
+    public void depositar(double depositarSaldo){
+        if(depositarSaldo > 0){
+            saldo += depositarSaldo;
+        }
+    }
+
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
+    public String getNome(){
+        return nome;
+    }
+
+    public double getSaldo(){
+        return saldo;
     }
 }
